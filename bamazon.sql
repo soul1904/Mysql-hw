d09ce7b4f9aa93c4a96f7338f5e5576d6b7d6bb5
soheil behzad <soheil_dark999@yahoo.com>
2019-06-24 10:04:44 -0700

    added a table in workbench on sql file

---
+++ bamazon.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE products_db;
+
+USE products_db;
+
+CREATE TABLE songs(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  artist VARCHAR(45) NULL,
+  genre VARCHAR(45) NULL,
+  PRIMARY KEY (id)
+);
